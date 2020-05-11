c8fff067e7d686789ea4192d32282ea5e2a760a0
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 17:14:25 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,15 +1,16 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-    :patriarch => {
-      :name => "Lord Montague",
-      :age => "53"
-  },
-  :capulet => {
-    :patriarch => {
-      :name => "Lord Capulet",
-      :age => "50"
-    }
-
-  
+     :montague => {
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
+     },
+     :capulet => {
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => {},
+        :heroine_friends => []
+     }
+  }
 end
